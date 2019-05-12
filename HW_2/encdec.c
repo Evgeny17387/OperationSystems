20ffa42ef34a9d064249901c9849aa34bb7438ab
Evgeny <evgeny17387@gmail.com>
2019-05-12 13:54:32 +0300

    Update

--- HW_2/encdec.c
+++ HW_2/encdec.c
@@ -1,3 +1,6 @@
+
+/************************************************************************* Original definitions and variables */
+
 #include <linux/ctype.h>
 #include <linux/config.h>
 #include <linux/module.h>
@@ -63,14 +66,24 @@ typedef struct {
 	int read_state;
 } encdec_private_date;
 
+/************************************************************************* Additional definitions and variables */
+
+#define SUCCESS 0
+#define ERROR -1
+
+char *buffer_caesar, *buffer_xor;
+
+/************************************************************************* Device methods implmentaion */
+
 int init_module(void)
 {
 
-	printk("init_module - start");
+	// Device registration
 
 	major = register_chrdev(major, MODULE_NAME, &fops_caesar);
 	if(major < 0)
 	{	
+	    printk("init_module.register_chrdev - FAIL !!!\n");
 		return major;
 	}
 
@@ -78,7 +91,33 @@ int init_module(void)
 	// -------------------------
 	// 1. Allocate memory for the two device buffers using kmalloc (each of them should be of size 'memory_size')
 
-	return 0;
+	// Memory allocation for caeser buffer
+
+    buffer_caesar = kmalloc(memory_size, GFP_KERNEL);
+
+    if(buffer_caesar == NULL)
+    {
+        printk("init_module.kmalloc.buffer_caesar - FAIL\n");
+        return ERROR;
+    }
+
+    memset(buffer_caesar, 0, memory_size);
+
+	// Memory allocation for xor buffer
+
+    buffer_xor = kmalloc(memory_size, GFP_KERNEL);
+
+    if(buffer_caesar == NULL)
+    {
+        printk("init_module.kmalloc.buffer_xor - FAIL\n");
+        return ERROR;
+    }
+
+    memset(buffer_xor, 0, memory_size);
+
+    printk("init_module - SUCCESS\n");
+
+	return SUCCESS;
 }
 
 void cleanup_module(void)
@@ -90,37 +129,169 @@ void cleanup_module(void)
 	unregister_chrdev(major, MODULE_NAME);
 
 	// 2. Free the allocated device buffers using kfree
+
+    kfree(buffer_caesar);
+    kfree(buffer_xor);
+
+    printk("cleanup_module - SUCCESS\n");
+
 }
+
+/************************************************************************* Common encription methods implmentaion */
 
 int encdec_open(struct inode *inode, struct file *filp)
 {
-//	int minor = MINOR(inode->i_rdev);
+
+	// Input arguments check
+
+	if (inode == NULL){
+        printk("encdec_open.inode - FAIL\n");
+		return ERROR;
+	}
+
+	if (filp == NULL){
+        printk("encdec_open.filp - FAIL\n");
+		return ERROR;
+	}
 
 	// Implemetation suggestion:
 	// -------------------------
 	// 1. Set 'filp->f_op' to the correct file-operations structure (use the minor value to determine which)
+
+	int minor = MINOR(inode->i_rdev);
+
+    if(minor == 0)
+    {
+        filp->f_op = &fops_caesar;
+    }
+    else if(minor == 1)
+    {
+        filp->f_op = &fops_xor;
+    }
+    else
+    {
+        printk("encdec_open.minor - FAIL\n");
+        return ERROR;
+    }
+
 	// 2. Allocate memory for 'filp->private_data' as needed (using kmalloc)
 
-	return 0;
+    encdec_private_date *private_data = kmalloc(sizeof(encdec_private_date), GFP_KERNEL);
+
+    if(private_data == NULL)
+    {
+        printk("encdec_open.kmalloc.private_data - FAIL\n");
+        return ERROR;
+    }
+
+    private_data->key = 0;
+    private_data->read_state = ENCDEC_READ_STATE_DECRYPT;
+
+    // Update file pointer
+
+    filp->private_data = private_data;
+
+    filp->f_pos = 0;
+
+    printk("encdec_open - SUCCESS\n");
+
+	return SUCCESS;
 }
 
 int encdec_release(struct inode *inode, struct file *filp)
 {
+
+	// Input arguments check
+
+	if (filp == NULL){
+        printk("encdec_open.filp - FAIL\n");
+		return ERROR;
+	}
+
 	// Implemetation suggestion:
 	// -------------------------
 	// 1. Free the allocated memory for 'filp->private_data' (using kfree)
 
-	return 0;
+    kfree(filp->private_data);
+
+    printk("encdec_release - SUCCESS\n");
+
+	return SUCCESS;
 }
 
 int encdec_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
 {
+
+	// Input arguments check
+
+	if (inode == NULL){
+        printk("encdec_open.inode - FAIL\n");
+		return ERROR;
+	}
+
+	if (filp == NULL){
+        printk("encdec_open.filp - FAIL\n");
+		return ERROR;
+	}
+
 	// Implemetation suggestion:
 	// -------------------------
 	// 1. Update the relevant fields in 'filp->private_data' according to the values of 'cmd' and 'arg'
 
-	return 0;
+	switch(cmd) {
+
+		case ENCDEC_CMD_CHANGE_KEY:
+
+			((encdec_private_date *)(filp->private_data))->key = (unsigned char)arg;
+
+			break;
+
+		case ENCDEC_CMD_SET_READ_STATE:
+
+			((encdec_private_date *)(filp->private_data))->read_state = (int)arg;
+
+			break;
+
+        case ENCDEC_CMD_ZERO:
+
+        {
+
+			int minor = MINOR(inode->i_rdev);
+
+            if(minor == 0)
+            {
+                memset(buffer_caesar, 0, memory_size);
+            }
+            else if(minor == 1)
+            {
+                memset(buffer_xor, 0, memory_size);
+            }
+		    else
+		    {
+		        printk("encdec_ioctl.ENCDEC_CMD_ZERO.minor - FAIL\n");
+		        return ERROR;
+		    }
+
+            filp->f_pos = 0;
+
+            break;
+
+        }
+
+		default:
+
+    		printk("encdec_ioctl - FAIL\n");
+
+			return ERROR;
+
+	}
+
+    printk("encdec_ioctl - SUCCESS\n");
+
+	return SUCCESS;
 }
+
+/************************************************************************* Specific encription methods implmentaion */
 
 // Add implementations for:
 // ------------------------
